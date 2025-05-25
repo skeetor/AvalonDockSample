61265b1dfa245919fcdf6b7f9028b6b964bee63f
Gerhard Gruber <sparhawk@gmx.at>
2025-05-25 19:56:45 +0200

    Initial Revision

---
+++ AvalonDockSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace AvalonDockSample.ViewModels
+{
+	class ViewModelBase : INotifyPropertyChanged
+	{
+
+		protected virtual void RaisePropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
